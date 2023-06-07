835319ba1e39ce0e6a2794a7ed4b3967d70b7646
Bjarke Berg <mail@bergmania.dk>
2023-06-07 15:09:56 +0200

    Updated version

--- src/Umbraco.IdentityExtensions/Properties/AssemblyInfo.cs
+++ src/Umbraco.IdentityExtensions/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Umbraco.Identity")]
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -22,6 +22,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("298754bc-c384-4195-9094-24940c569e6a")]
 
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
 [assembly: AssemblyCopyright("Copyright © Umbraco 2019")]
